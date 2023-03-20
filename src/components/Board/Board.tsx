472a4991f191c733f6bcecfeecd6ff97dc828990
Victor Andersson <vabandersson@hotmail.com>
2023-03-20 17:08:02 +0100

    first commit, making a kanban board

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Board.scss";
+import Ticket from "../Ticket/Ticket";
+export default function Board() {
+    return (
+        <div className="board">
+            <div className="board-column">
+                <div className="board-title">
+                    <div className="board-title-icon todo"></div>
+                    <h4>Todo</h4>
+                </div>
+                <Ticket />
+                <Ticket />
+                <Ticket />
+            </div>
+            <div className="board-column">
+                <div className="board-title">
+                    <div className="board-title-icon doing"></div>
+                    <h4>Doing</h4>
+                </div>
+                <Ticket />
+                <Ticket />
+                <Ticket />
+            </div>
+            <div className="board-column">
+                <div className="board-title">
+                    <div className="board-title-icon done"></div>
+                    <h4>Done</h4>
+                </div>
+                <Ticket />
+                <Ticket />
+                <Ticket />
+            </div>
+        </div>
+    );
+}
